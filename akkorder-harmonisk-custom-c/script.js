543bdaa8b422bd8aeab15f2310f14665b4f63a42
Fredrik Blekk <26469320+gosh-darn@users.noreply.github.com>
2025-08-12 15:14:39 +0200

    Vwesion 16.3
    Fjernet melodiske akkorder og moder da disse virket litt dårlig gjennomførte. Forbedret lydkvalitet på harmoniske akkorder

--- akkorder-harmonisk-custom-c/script.js
+++ akkorder-harmonisk-custom-c/script.js
@@ -50,7 +50,7 @@ function getRandomFilePath() {
   previousAnswer = randomIndex;
   currentAnswer = randomIndex;
 
-  currentFilePath = `../lyd-akkorder-harmonisk-c/${String(randomIndex).padStart(2, '0')}.opus`;
+  currentFilePath = `../lyd-akkorder-harmonisk/${String(randomIndex).padStart(2, '0')}g.opus`;
 
   return currentFilePath;
 }
